ac2f86fc9ce253b88d2ce9b5815bedb6bd24ef74
bhahirathi262@gmail.com <102673355+Bhahirathy@users.noreply.github.com>
2023-08-17 20:25:22 +0530

    weatherapi integrated

--- src/Component/landingPage.js
+++ src/Component/landingPage.js
@@ -5,6 +5,47 @@ import Umbrella from "../assets/images/icons/icon-umberella.png";
 import Wind from "../assets/images/icons/icon-wind.png";
 import Compass from "../assets/images/icons/icon-compass.png";
 export default function LandingPage() {
+    const [city, setCity] = useState("")
+    const [location, setLocation] = useState({ name: "", latitude: "", longitude: "", description: "" });
+    const [currentWeather, setCurrentWeather] = useState(null);
+    const [forecast, setForecast] = useState([]);
+    const getWeatherApi = async () => {
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(async position => {
+                const latitude = position.coords.latitude;
+                const longitude = position.coords.longitude;
+                const apiKey = "efe5e49a8f9903142b34845847c5af83";
+                const weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+                const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`
+
+                try {
+                    const response = await axios.get(currentWeatherUrl);
+                    if (response.status === 200) {
+                        console.log("hftyf", response.data);
+                        setLocation({
+                            name: response.data.name,
+                            description: response.data.weather.map((data) => {
+                                return data.description
+                            }),
+                            latitude,
+                            longitude,
+
+                        });
+                        setCurrentWeather(response.data.dt)
+
+                    }
+                } catch (error) {
+                    console.log(error);
+                }
+            });
+        }
+
+    };
+
+
+    useEffect(() => {
+        getWeatherApi();
+    }, []);
 
     return (
         <>
@@ -23,11 +64,11 @@ export default function LandingPage() {
                         <div class="today forecast">
                             <div class="forecast-header">
                                 <div class="day">Monday</div>
-                                <div class="date">6 Oct</div>
+                                <div class="date">{new Date(parseInt(currentWeather?.dt) * 1000).toLocaleDateString()}</div>
                             </div>
                             {/* <!-- .forecast-header --> */}
                             <div class="forecast-content">
-                                <div class="location">New York</div>
+                                <div class="location">{location.name}</div>
                                 <div class="degree">
                                     <div class="num">23<sup>o</sup>C</div>
                                     <div class="forecast-icon">
@@ -36,10 +77,10 @@ export default function LandingPage() {
                                 </div>
                                 <span><img src={Umbrella} alt="" />20%</span>
                                 <span><img src={Wind} alt="" />18km/h</span>
-                                <span><img src={Compass} alt="" />East</span>
+                                <span><img src={Compass} alt="" />{location.description}</span>
                             </div>
                         </div>
-                        <div class="forecast">
+                        {<div class="forecast">
                             <div class="forecast-header">
                                 <div class="day">Tuesday</div>
                             </div>
@@ -51,7 +92,7 @@ export default function LandingPage() {
                                 <div class="degree">23<sup>o</sup>C</div>
                                 <small>18<sup>o</sup></small>
                             </div>
-                        </div>
+                        </div>}
                         <div class="forecast">
                             <div class="forecast-header">
                                 <div class="day">Wednesday</div>
