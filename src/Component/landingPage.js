bd1d9937af8ae573a52b2f5eb66d4d2a89990375
bhahirathi262@gmail.com <102673355+Bhahirathy@users.noreply.github.com>
2023-08-18 12:38:49 +0530

    hourly search forcast integrated

--- src/Component/landingPage.js
+++ src/Component/landingPage.js
@@ -1,18 +1,17 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import ForecastIcon from "../assets/images/icons/icon-1.svg";
 import Umbrella from "../assets/images/icons/icon-umberella.png";
 import Wind from "../assets/images/icons/icon-wind.png";
 import Compass from "../assets/images/icons/icon-compass.png";
 import Day from "../assets/images/day.jpg"
 export default function LandingPage() {
     const [city, setCity] = useState("")
-    const [location, setLocation] = useState({ name: "", latitude: "", longitude: "", description: "" });
+    const [location, setLocation] = useState({ name: "", country: "", latitude: "", longitude: "", description: "" });
     const [currentWeather, setCurrentWeather] = useState(null);
     const [temperature, setTemperature] = useState("");
     const [icon, setIcon] = useState("")
     const [wind, setWind] = useState("")
-    const [forecast, setForecast] = useState([]);
+    const [humidity, setHumidity] = useState("")
     const [forecastData, setForecastData] = useState(null);
     const [forecastHrsData, setForecastHrsData] = useState(null);
     const [dark, white] = useState(false)
@@ -23,15 +22,14 @@ export default function LandingPage() {
                 const latitude = position.coords.latitude;
                 const longitude = position.coords.longitude;
                 const apiKey = "efe5e49a8f9903142b34845847c5af83";
-                const weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
                 const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`
 
                 try {
                     const response = await axios.get(currentWeatherUrl);
                     if (response.status === 200) {
-                        // console.log("hftyf", response.data);
                         setLocation({
                             name: response.data.name,
+                            country: response.data.sys.country,
                             description: response.data.weather.map((data) => {
                                 return data.description
                             }),
@@ -45,6 +43,7 @@ export default function LandingPage() {
                             return data.icon
                         }))
                         setWind(response.data.wind.speed)
+                        setHumidity(response.data.main.humidity)
 
                     }
                 } catch (error) {
@@ -66,11 +65,9 @@ export default function LandingPage() {
                 try {
                     const response = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&cnt=${days}&appid=${apiKey}`);
                     if (response.status === 200) {
-                        // setForecastData(response.data);
-                        // console.log("fore", response.data)
                         const dailyForecast = response.data.list.filter((item, index) => index % 8 === 0);
                         setForecastData(dailyForecast);
-                        // console.log(dailyForecast)
+                        console.log(dailyForecast)
                     }
                 } catch (error) {
                     console.error('Error fetching forecast data:', error);
@@ -89,8 +86,7 @@ export default function LandingPage() {
                 try {
                     const response = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&cnt=48&appid=${apiKey}`);
                     if (response.status === 200) {
-                        setForecastHrsData(response.data);
-                        // console.log("forehrs", response.data)
+                        setForecastHrsData(response.data.list);
                     }
                 } catch (error) {
                     console.error('Error fetching forecast data:', error);
@@ -101,44 +97,47 @@ export default function LandingPage() {
 
     const searchWeather = async (e) => {
         e.preventDefault()
-        console.log("button")
-        const days = 8 * 8
+        const days = 7 * 8
+        const hoursInterval = 3;
 
         if (city) {
             try {
-
                 const apiKey = "efe5e49a8f9903142b34845847c5af83";
-                const weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&cnt=${days}&appid=${apiKey}`;
+                const weatherURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&cnt=${days}&appid=${apiKey}`;
                 const response = await axios.get(weatherURL, {
                 });
-                // setForecast(response.data)
                 console.log("search", response.data)
 
                 setLocation({
-                    name: response.data.name,
-                    description: response.data.weather.map((data) => {
+                    name: response.data.city.name,
+                    country: response.data.sys.country,
+                    description: response.data.list[0].weather.map((data) => {
                         return data.description
                     }),
+                    latitude: response.data.city.coord.lat,
+                    longitude: response.data.city.coord.lon,
 
                 });
-                setCurrentWeather(response.data.dt)
-                setTemperature(response.data.main.temp)
-                setIcon(response.data.weather.map((data) => {
+                setCurrentWeather(response.data.list[0].dt)
+                setTemperature((response.data.list[0].main.temp - 273.15).toFixed(2))
+                setIcon(response.data.list[0].weather.map((data) => {
                     return data.icon
                 }))
-                setWind(response.data.wind.speed)
+                setWind(response.data.list[0].wind.speed)
                 const dailyForecast = response.data.list.filter((item, index) => index % 8 === 0);
                 setForecastData(dailyForecast);
+                const hourlyForecast = response.data.list.filter((item, index) => index % hoursInterval === 0);
+                setForecastHrsData(hourlyForecast);
+                console.log(hourlyForecast)
             } catch (error) {
                 console.error('Error fetching forecast data:', error);
             }
         }
     };
 
-    const handleTheme = () => {
-        console.log(dark)
-        white(!dark)
 
+    const handleTheme = () => {
+        white(!dark)
     }
 
     useEffect(() => {
@@ -148,8 +147,11 @@ export default function LandingPage() {
     return (
         <>
             <div className={dark ? 'dark' : 'white'}>
+                <h2 style={{ textAlign: "center", padding: "10px", color: "#dad19b" }}>Weather Forecast</h2>
                 <div className="hero" data-bg-image={Day}>
-                    <button onClick={handleTheme}>Theme</button>
+                    <span className="icon" onClick={handleTheme}>
+                        {dark ? '☀️' : '🌙'}
+                    </span>
                     <div className="container">
                         <form onSubmit={searchWeather} className="find-location">
                             <input type="text" placeholder="Find your location..." value={city} onChange={e => setCity(e.target.value)} ></input>
@@ -165,17 +167,16 @@ export default function LandingPage() {
                                     <div className="day">{new Date(currentWeather * 1000).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}</div>
                                     <div className="date">{new Date(currentWeather * 1000).toLocaleDateString('en-US', { weekday: 'long' })}</div>
                                 </div>
-                                {/* <!-- .forecast-header --> */}
                                 <div className="forecast-display">
                                     <div className="forecast-content">
-                                        <div className="location">{location.name}</div>
+                                        <div className="location">{location.name},{location.country}</div>
                                         <div className="degree">
                                             <div className="num">{temperature}<sup>o</sup>C</div>
                                             <div className="forecast-icon">
                                                 <img src={`http://openweathermap.org/img/w/${icon}.png`} alt="" width="90" />
                                             </div>
                                         </div>
-                                        <span><img src={Umbrella} alt="" />20%</span>
+                                        <span><img src={Umbrella} alt="" />{humidity}%</span>
                                         <span><img src={Wind} alt="" />{wind}km/h</span>
                                         <span><img src={Compass} alt="" />{location.description}</span>
                                     </div>
@@ -199,16 +200,20 @@ export default function LandingPage() {
                                                 </div>
                                                 <div className="degree">{(val.main.temp - 273.15).toFixed(2)}<sup>o</sup>C</div>
                                                 <small>{val.main.humidity}%</small>
+
                                             </div>
                                         </div>
                                     </>
                                 )
 
                             })}
+                            <div className="heading-section">
+                                <h4 className="heading" style={{ textAlign: "center", marginTop: "30px" }}>Hourly Forecast for next 48 HRS</h4>
+                                <div className="clear"></div>
+                            </div>
                             <br />
 
-                            <h4 style={{ textAlign: "center" }}>Hourly Forecast for next 48 HRS</h4>
-                            {forecastHrsData && forecastHrsData?.list.map((val, index) => {
+                            {forecastHrsData && forecastHrsData?.map((val, index) => {
                                 const date = new Date(val.dt * 1000);
                                 const time = date.toLocaleTimeString('en-US', { hour: 'numeric', hour12: true });
                                 return (
@@ -218,7 +223,6 @@ export default function LandingPage() {
                                             <div className="forecast-header">
                                                 <div className="day">{val.dt_txt}</div>
                                             </div>
-                                            {/* <!-- .forecast-header --> */}
                                             <div className="forecast-content">
                                                 <div className="forecast-icon">
                                                     <img src={`http://openweathermap.org/img/w/${val.weather[0].icon}.png`} alt="" width="48" />
@@ -229,10 +233,10 @@ export default function LandingPage() {
                                         </div>
                                     </>
                                 )
-                                // }
-                                // return null
+
 
                             })}
+
 
                         </div>
 
